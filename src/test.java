6130d726ed01e1805839b2e6a7887b9a70d00999
JsHaaaaaan <gkswhdtjr000802@gmail.com>
2025-05-27 09:50:03 +0900

    modified_test.java

--- src/test.java
+++ src/test.java
@@ -2,5 +2,8 @@ import java.io.*;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+
     }
 }
